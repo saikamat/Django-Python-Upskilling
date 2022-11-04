ba12c6ea2d63c6191967a895c593475dd0e4a91e
saikamat <sai.s.kamat@gmail.com>
2022-11-04 12:25:39 +0100

    interactive color play

---
+++ html-practice/js/02-change-colors.js
@@ -0,0 +1,16 @@
+function greenClick(){
+    // alert("Green Click!")
+    document.getElementById('Block-1').innerHTML = "So make it lighter!"
+    document.body.style.backgroundColor="lightgreen";
+}
+
+function orangeClick(){
+    // alert("Orange Click!");
+    document.getElementById('Block-2').innerHTML = "So make it fiery!"
+    document.body.style.backgroundColor="orange";
+}
+
+function reset(){
+    // alert("Reset!");
+    document.body.style.backgroundColor="white";
+}
