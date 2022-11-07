d28edaa9a7c78e1752cbf6c47ead7665f3b9285c
saikamat <sai.s.kamat@gmail.com>
2022-11-07 14:29:06 +0100

    counter v3 - change background with each change

--- html-practice/js/03-counter.js
+++ html-practice/js/03-counter.js
@@ -1,13 +1,24 @@
-var num=0;
+var colour=0xef5140;
+
+function dec2Hex(dec) {
+    return Math.abs(dec).toString(16);
+}
 
 function increment(){
-    num=num+1;
-    // alert(num)
-    document.getElementById('value').innerHTML=num;
+    colour=colour+50;
+    // alert(dec2Hex(num))
+    // document.getElementById('value').innerHTML=num;
+    // document.body.style.backgroundColor=dec2Hex(colour);
+    // document.body.style.backgroundColor="#ef5140";
+    // alert(#+(dec2Hex(num)))
+    // console.log('#'+dec2Hex(colour));
+    document.body.style.backgroundColor='#'+dec2Hex(colour);
 }
 
 function decrement(){
-    num=num-1;
-    // alert(num)
-    document.getElementById('value').innerHTML=num;
+    colour=colour-50;
+    // alert(dec2Hex(num))
+    // document.getElementById('value').innerHTML=num;
+    // document.body.style.backgroundColor=dec2Hex(colour);
+    document.body.style.backgroundColor='#'+dec2Hex(colour);
 } 
