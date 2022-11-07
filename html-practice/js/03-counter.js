5072aab88a675e8d8010bd8420eac464ddec1fbf
saikamat <sai.s.kamat@gmail.com>
2022-11-07 13:09:07 +0100

    try counter v1

---
+++ html-practice/js/03-counter.js
@@ -0,0 +1,13 @@
+var num=0;
+
+function increment(num){
+    num=num+1;
+    // alert(num)
+    document.getElementById('value').innerHTML=num;
+}
+
+function decrement(num){
+    num=num-1;
+    // alert(num)
+    document.getElementById('value').innerHTML=num;
+} 
