7ffa3de54f0be2f0faaa5722b28f190f8d527aef
saikamat <sai.s.kamat@gmail.com>
2022-11-09 18:07:20 +0100

    animation with jQuery

---
+++ html-practice/js/08-animate-with-jq.js
@@ -0,0 +1,10 @@
+$('#animate-block').animate({
+    height: '250px',
+    width: '500px',
+    opacity: 1
+}, 2000);
+
+
+// $('#animate-block').click(function() {console.log('Clicked.')}) 
+
+$("#animate-block").animate({left: "590px"});
