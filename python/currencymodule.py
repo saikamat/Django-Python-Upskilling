71c4a4c89e31d2d82149c1450f712cf77e342428
saikamat <sai.s.kamat@gmail.com>
2022-11-10 18:50:47 +0100

    creating a custom python module v1

---
+++ python/currencymodule.py
@@ -0,0 +1,12 @@
+# %%
+def getCurrentTime():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return now
+
+# %%
+def getBuyValue(currency_type):
+    buy_value = requests.get('https://blockchain.info/ticker').json()[currency_type]['buy']
+    print('{} buy value at {} is {}'.format(currency_type, getCurrentTime(), buy_value))
+
+
