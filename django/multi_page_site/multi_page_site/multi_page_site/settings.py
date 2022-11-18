2ee3c8c0173b8a57111995caa534f2cbc3a30262
saikamat <sai.s.kamat@gmail.com>
2022-11-18 20:25:11 +0530

    multipage - 04 add css to mix

--- django/multi_page_site/multi_page_site/multi_page_site/settings.py
+++ django/multi_page_site/multi_page_site/multi_page_site/settings.py
@@ -117,8 +117,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+from os.path import join
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (join('static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
