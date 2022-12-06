8550521957bba03e26c869e7bab206eb6e6ba21b
saikamat <sai.s.kamat@gmail.com>
2022-12-06 19:11:51 +0530

    moving and displaying data between pages

--- django/titanic_survivor_predictor/titanic_survivor_predictor/titanic_survivor_predictor/views.py
+++ django/titanic_survivor_predictor/titanic_survivor_predictor/titanic_survivor_predictor/views.py
@@ -4,4 +4,5 @@ def home(request):
     return render(request, 'index.html')
 
 def results(request):
-    return render(request, 'results.html')
+    user_input_age = request.GET["age"]
+    return render(request, 'results.html', {'age': user_input_age})
