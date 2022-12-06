31e3ab18c0b22d655858b5867f51c1a4de5adcf9
saikamat <sai.s.kamat@gmail.com>
2022-12-06 20:00:38 +0530

    a simple python function

---
+++ django/titanic_survivor_predictor/titanic_survivor_predictor/titanic_survivor_predictor/fake_model.py
@@ -0,0 +1,6 @@
+def fake_predict(age):
+    if age < 10:
+        age_prediction = "Survived"
+    else:
+        age_prediction = "May God Bless their souls."
+    return age_prediction
--- django/titanic_survivor_predictor/titanic_survivor_predictor/titanic_survivor_predictor/views.py
+++ django/titanic_survivor_predictor/titanic_survivor_predictor/titanic_survivor_predictor/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from . import fake_model
 
 def home(request):
     return render(request, 'index.html')
 
 def results(request):
-    user_input_age = request.GET["age"]
-    return render(request, 'results.html', {'age': user_input_age})
+    user_input_age = int(request.GET["age"])
+    age_predictor = fake_model.fake_predict(user_input_age)
+    return render(request, 'results.html', {'age_prediction': age_predictor})
