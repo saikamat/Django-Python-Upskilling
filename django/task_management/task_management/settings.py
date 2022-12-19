f554343b809a245174777f97b6bf6d826b33b3a4
saikamat <sai.s.kamat@gmail.com>
2022-12-19 20:24:09 +0100

    setting up basics for django DB

--- django/task_management/task_management/settings.py
+++ django/task_management/task_management/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'task_management_app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'task_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['task_management_app/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
