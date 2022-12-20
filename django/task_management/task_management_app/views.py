80ba77724624eb4932b3a64a50134bd8e0245c44
saikamat <sai.s.kamat@gmail.com>
2022-12-20 10:46:05 +0100

    connect database to webpage

--- django/task_management/task_management_app/views.py
+++ django/task_management/task_management_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import TaskDb
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    all_items = TaskDb.objects.all()
+    return render(request, 'index.html', {'all_items':all_items})
